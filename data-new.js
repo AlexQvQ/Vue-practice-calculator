9139783073ccd37327fed3e2dfe8dff2de406740
Alex <100189060+AlexQvQ@users.noreply.github.com>
2024-02-23 19:47:15 +0300

    Add files via upload

---
+++ data-new.js
@@ -0,0 +1,23 @@
+let data = 
+{
+   bankOne: {
+      title: "Крутой банк",
+      char: 7
+   },
+   bankTwo: {
+      title: "Банк",
+      char: 7.5
+   },
+   bankThree: {
+      title: "Вообще крутой банк",
+      char: 6
+   },
+   bankFour: {
+      title: "Не очень банк",
+      char: 8
+   },
+   bankFour: {
+      title: "Не очень банк ffff",
+      char: 9
+   }
+}
